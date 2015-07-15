e772ec671fad2c39b768034ccee25831ac41fab7
Sandro Marques <sandro.marques.comp@gmail.com>
2015-07-15 13:28:32 +0000

    Implementação do Bootstrap

---
+++ config/initializers/superfeedr_engine.rb
@@ -0,0 +1,7 @@
+SuperfeedrEngine::Engine.feed_class = "Feed"
+
+SuperfeedrEngine::Engine.host = "cryptic-peak-7737.herokuapp.com"
+SuperfeedrEngine::Engine.login = "demo"
+SuperfeedrEngine::Engine.password = "8ac38a53cc32f71a6445e880f76fc865"
+SuperfeedrEngine::Engine.scheme = "http"
+SuperfeedrEngine::Engine.base_path = "/secret_path_for_superfeedr/" # don't forget the trailing /
